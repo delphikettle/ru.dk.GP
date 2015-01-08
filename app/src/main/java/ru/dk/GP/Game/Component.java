8fea4bd26593d4910fe720236fb758db1117d36e
delphikettle <delphikettle@gmail.com>
2015-01-08 12:57:49 +1000

    15_01_08_12_57

--- app/src/main/java/ru/dk/GP/Game/Component.java
+++ app/src/main/java/ru/dk/GP/Game/Component.java
@@ -3,22 +3,38 @@ package ru.dk.GP.Game;
 public abstract class Component
 {
 	private volatile float vx,vy;
-	private volatile Float x,y;
+	private volatile float x,y;
 	private float m;
 	private Particle owner;
+	private boolean isNeedRecount=true;
+	private float lastF;
 	public Component(float x, float y,float m){
 		this.x=x;
 		this.y=y;
 		this.vx=0;
 		this.vy=0;
+		this.m=m;
+		owner=null;
 	}
 	public Component(Particle p){
-		x=null;
-		y=null;
+		x=0;
+		y=0;
 		this.owner=p;
 	}
 	public void nextStep(){
-		x+=vx;
-		y+=vy;
+		if(owner==null){
+			x+=vx;
+			y+=vy;
+		}else{
+			
+		}
+	}
+	public float getF(){
+		if(isNeedRecount)return lastF=recountF();else
+			return lastF;
+	}
+	private float recountF(){
+		isNeedRecount=false;
+		return m;
 	}
 }
--- app/src/main/java/ru/dk/GP/Game/Level.java
+++ app/src/main/java/ru/dk/GP/Game/Level.java
@@ -1,6 +1,9 @@
 package ru.dk.GP.Game;
 
-public class Level extends Thread
+public abstract class Level extends Thread
 {
+	static public void Interaction(Component c1, Component c2){
+		float F= c1.getF()*c2.getF();
+	}
 	
 }
