e84db14a72b18c5260f4460bf75a1bc7e7fe36a3
delphikettle <delphikettle@gmail.com>
2015-01-09 21:57:09 +0300

    15_01_08_21_57

--- app/src/main/java/ru/dk/GP/Game/Level.java
+++ app/src/main/java/ru/dk/GP/Game/Level.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import static ru.dk.GP.Game.Component.getDistance;
 
-public abstract class Level extends Thread
+public abstract class Level extends Thread implements Runnable
 {
 	ArrayList<Component> particles;
 	//borders:
@@ -24,7 +24,7 @@ public abstract class Level extends Thread
 		this.setDaemon(true);
 	}
 
-	private void Interaction(Component c1, Component c2){
+	synchronized private void Interaction(Component c1, Component c2){
 		float d=getDistance(c1,c2);
 		float F= (float) (c1.getF()*c2.getF()/Math.pow(d,2));
 		float F1=F/c1.getM(),F2=F/c2.getM();
@@ -33,18 +33,18 @@ public abstract class Level extends Thread
 		c1.informXAcceleration(F1*Component.getYDiff(c1, c2)/d);
 		c2.informXAcceleration(F2*Component.getYDiff(c2, c1)/d);
 	}
-	private void Move(float time){
-		for(int i=0;i<particles.size();i++)
-			if(particles.get(i) != null){
-			for(int j=i+1;i<particles.size();j++)
-				if(particles.get(j)!=null)
-					this.Interaction(particles.get(i),particles.get(j));
-			particles.get(i).nextStep(time);
+	synchronized private void Move(float time){
+		for(int i=0;i<particles.size();i++) {
+			try {
+				for (int j = i + 1; i < particles.size(); j++)
+					this.Interaction(particles.get(i), particles.get(j));
+				particles.get(i).nextStep(time);
+			}catch (NullPointerException e){particles.remove(i);}
 		}
 	}
 
-	private float getNextStepTime(){
-		return (float)((currentGameTime=(this.timeFactor*(-currentRealTime+(currentRealTime=System.currentTimeMillis())))+currentGameTime)-currentGameTime);
+	synchronized private float getNextStepTime(){
+		return (float)((currentGameTime=(this.timeFactor*(-currentRealTime+(currentRealTime=System.currentTimeMillis())))+currentGameTime                              )-currentGameTime);
 	}
 
 	@Override
